ce8b9cee2452704d8f5eefcd5229e390335f0b64
Somakodi School <info.somakodi@gmail.com>
2026-07-07 10:31:15 +0300

    domesticworker.html
    created nanny cards

--- js/script.js
+++ js/script.js
@@ -5,3 +5,38 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+
+function openProfile(
+    name,
+    image,
+    age,
+    town,
+    type,
+    salary,
+    experience,
+    skills
+){
+
+    document.getElementById("profileImage").src = image;
+    document.getElementById("profileName").innerHTML = name;
+    document.getElementById("profileAge").innerHTML = age;
+    document.getElementById("profileTown").innerHTML = town;
+    document.getElementById("profileType").innerHTML = type;
+    document.getElementById("profileSalary").innerHTML = salary;
+    document.getElementById("profileExperience").innerHTML = experience;
+    document.getElementById("profileSkills").innerHTML = skills;
+
+    document.getElementById("profileModal").style.display = "flex";
+}
+
+function closeProfile(){
+    document.getElementById("profileModal").style.display = "none";
+}
+
+window.onclick = function(event){
+    let modal = document.getElementById("profileModal");
+
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
